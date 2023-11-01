39c3d27af800428e3f40b9939138ba087c59e614
MAIA KUPERSMID <48045053@ort>
2023-11-01 09:55:43 -0300

    maiu juli

--- Models/Piercing.cs
+++ Models/Piercing.cs
@@ -4,14 +4,16 @@ public class Piercing{
     public string Material;
     public string Color;
     public string Foto;
+    public string Nombre;
 
     public Piercing(){}
 
-    public Piercing(int idPiercing, int precio, string material, string color, string foto){
+    public Piercing(int idPiercing, int precio, string material, string color, string foto, string nombre){
         IdPiercing=idPiercing;
         Precio=precio;
         Material=material;
         Color=color;
         Foto=foto;
+        Nombre = nombre;
     }
 }

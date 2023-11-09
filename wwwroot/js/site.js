d37339f3d4c33bc1af643a7d91c27074f109d1aa
JULIETA IZBICKI <47350196@ort>
2023-11-09 14:22:37 -0300

    TERMINE AJAX

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -44,8 +44,7 @@ function Likear(id)
         data: {id},
 
         success: function(like) {             
-            window.location.reload;
+            window.location.reload();
         }
     });
-
 }
